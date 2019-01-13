463362c066668a192cbb72a937319cc3d083201f
Gabriel <gabogsc@gmail.com>
2019-01-13 18:41:04 -0300

    Git flow test

--- src/main/java/com/fingeso/backend/BackendApplication.java
+++ src/main/java/com/fingeso/backend/BackendApplication.java
@@ -9,6 +9,7 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		System.out.println("Test\n");
 		SpringApplication.run(BackendApplication.class, args);
+		System.out.println("gitflow");
 	}
 
 }
