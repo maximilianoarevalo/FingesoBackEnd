58602fd6d5c746d12e60a731d05484b84eb07c50
Gabriel <gabogsc@gmail.com>
2019-01-13 18:42:39 -0300

    Git flow realease

--- src/main/java/com/fingeso/backend/BackendApplication.java
+++ src/main/java/com/fingeso/backend/BackendApplication.java
@@ -10,6 +10,7 @@ public class BackendApplication {
 		System.out.println("Test\n");
 		SpringApplication.run(BackendApplication.class, args);
 		System.out.println("gitflow");
+		System.out.println("notas reales gitflow");
 	}
 
 }
