366f1948431bea4555fbc9c012724da5f42bd003
Unknown <gabriel.gonzalez.s@usach.cl>
2019-01-14 01:39:48 -0300

    Crud Comentarios

--- src/main/java/com/fingeso/backend/Repository/ComentarioRepository.java
+++ src/main/java/com/fingeso/backend/Repository/ComentarioRepository.java
@@ -1,4 +1,10 @@
 package com.fingeso.backend.Repository;
 
-public interface ComentarioRepository {
+import com.fingeso.backend.Models.Comentario;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface ComentarioRepository extends MongoRepository<Comentario, String> {
+    Comentario findBy_id(ObjectId _id);
 }
