31b30cf13c994cc3b643e04c14548786bc0c7951
Maximiliano Arévalo Sáez <maximiliano.arevalo@usach.cl>
2019-01-13 22:58:51 -0300

    Arreglos Desafio
    -Se modifica Desafio, DesafioRepository, DesafioService
    -Se prueba en postman
    -Se realiza una arreglo en Idea

--- src/main/java/com/fingeso/backend/Models/Desafio.java
+++ src/main/java/com/fingeso/backend/Models/Desafio.java
@@ -1,21 +1,52 @@
 package com.fingeso.backend.Models;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+//@Document(collection = "desafios")
 public class Desafio {
-
-
-
-
-    private Usuario Autor;
+    @Id
+    private ObjectId _id;
+    private String nombre;
     private String descripcion;
-    private int id_desafio;
-    private String fechaPublicacion;
-    private ArrayList<Idea> listaIdeas;
+    private LocalDateTime fechaPublicacion;
+    //Falta agregar quien creo que desafio
+    //Falta agregar las ideas pertenecientes a este desafio
 
+    public Desafio(){}
 
+    public Desafio(ObjectId _id, String nombre, String descripcion){
+        this._id = _id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fechaPublicacion = LocalDateTime.now();
+    }
 
+    public String get_id(){
+        return _id.toHexString();
+    }
 
+    public void set_id(ObjectId _id){
+        this._id = _id;
+    }
 
+    public String getNombre(){
+        return nombre;
+    }
 
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion(){
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion){
+        this.descripcion = descripcion;
+    }
 }
