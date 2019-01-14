07914120332b68d481a004299bd8613e4edc445c
José Manuel Ibaceta Jaña <jose.ibaceta@usach.cl>
2019-01-14 09:08:47 -0300

    Usuario
    Implementacion de metodos y atributos de "usuario"

--- src/main/java/com/fingeso/backend/Models/Usuario.java
+++ src/main/java/com/fingeso/backend/Models/Usuario.java
@@ -2,20 +2,56 @@ package com.fingeso.backend.Models;
 
 import java.util.ArrayList;
 import java.util.Date;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 
 public class Usuario {
-
-
-
-
-
+    @Id
+    private ObjectId _id;
     private String Nombres;
     private String Apellidos;
     private Date fechaNacimiento;
     private String email;
     private ArrayList<Idea> listaIdeas;
 
-
+    public Usuario(ObjectId _id, String Nombres, String Apellidos,Date fechaNacimiento,String email){
+        this._id = _id;
+        this.Nombres = Nombres;
+        this.Apellidos = Apellidos;
+        this.fechaNacimiento=fechaNacimiento;
+        this.email=email;
+        this.listaIdeas=new ArrayList<Idea>();
+    }
+    public String get_id(){
+        return _id.toHexString();
+    }
+    public void set_id(ObjectId _id){
+        this._id = _id;
+    }
+    public String getNombres(){
+        return Nombres;
+    }
+    public void setNombres(String nombre){
+        this.Nombres = Nombres;
+    }
+    public String getApellidos(){
+      return Apellidos;
+    }
+    public void setApellidos(String apellidos){
+      this.Apellidos= Apellidos;
+    }
+    public Date getFechaNacimiento(){
+      return fechaNacimiento;
+    }
+    public void setFechaNacimiento(Date fechaNacimiento){
+      this.fechaNacimiento=fechaNacimiento;
+    }
+    public String getEmail(){
+      return email;
+    }
+    public void setEmail(String email){
+      this.email=email;
+    }
     public void crearIdea(){
 
     }
--- src/main/java/com/fingeso/backend/Services/UsuarioService.java
+++ src/main/java/com/fingeso/backend/Services/UsuarioService.java
@@ -1,4 +1,43 @@
 package com.fingeso.backend.Services;
 
+import com.fingeso.backend.Models.Usuario;
+import com.fingeso.backend.Repository.UsuarioRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/usuarios")
 public class UsuarioService {
+  @Autowired
+  private UsuarioRepository repository;
+
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public List<Usuario> getAllUsuario(){
+      return repository.findAll();
+  }
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public Usuario getUsuarioById(@PathVariable("id") ObjectId id){
+      return repository.findBy_id(id);
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+  public void modifyUsuarioById(@PathVariable("id") ObjectId id, @Valid@RequestBody Usuario usuario){
+      usuario.set_id(id);
+      repository.save(usuario);
+  }
+
+  @RequestMapping(value = "/", method = RequestMethod.POST)
+  public Usuario createUsuario(@Valid @RequestBody Usuario usuario){
+      usuario.set_id(ObjectId.get());
+      repository.save(usuario);
+      return usuario;
+  }
+
+  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+  public void deleteUsuario(@PathVariable ObjectId id){
+      repository.delete(repository.findBy_id(id));
+  }
 }
